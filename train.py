ca6b0fb93d8e1b4a96c061d304137445789719bb
jj-ctl <jj-ctl>
2022-04-10 12:48:03 +0200

    added the requested code

--- train.py
+++ train.py
@@ -43,7 +43,8 @@ def main():
 
     parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
     parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
-
+    lucky_number = 42
+    test_size=0.2
     args = parser.parse_args()
 
     run = Run.get_context()
@@ -55,13 +56,13 @@ def main():
     # Data is located at:
     # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-    ds = ### YOUR CODE HERE ###
+    ds =TabularDatasetFactory.from_delimited_files("https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv")
     
     x, y = clean_data(ds)
 
     # TODO: Split data into train and test sets.
 
-    ### YOUR CODE HERE ###a
+    x_train, x_test, y_train, y_test =train_test_split(x,y,test_size=test_size, random_state=lucky_number)
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
