f457865bce18bce8c3b448de5f1d67be3a669016
jj-ctl <jj-ctl>
2022-04-10 15:02:43 +0200

    added automl

--- train.py
+++ train.py
@@ -10,10 +10,27 @@ import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
+LUCKY_NUMBER = 42
+TEST_SIZE = 0.2
+
+
 def clean_data(data):
     # Dict for cleaning data
-    months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
-    weekdays = {"mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6, "sun":7}
+    months = {
+        "jan": 1,
+        "feb": 2,
+        "mar": 3,
+        "apr": 4,
+        "may": 5,
+        "jun": 6,
+        "jul": 7,
+        "aug": 8,
+        "sep": 9,
+        "oct": 10,
+        "nov": 11,
+        "dec": 12,
+    }
+    weekdays = {"mon": 1, "tue": 2, "wed": 3, "thu": 4, "fri": 5, "sat": 6, "sun": 7}
 
     # Clean and one hot encode data
     x_df = data.to_pandas_dataframe().dropna()
@@ -37,14 +54,24 @@ def clean_data(data):
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
     return x_df, y_df
 
+
 def main():
     # Add arguments to script
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
-    parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
-    lucky_number = 42
-    test_size=0.2
+    parser.add_argument(
+        "--C",
+        type=float,
+        default=1.0,
+        help="Inverse of regularization strength. Smaller values cause stronger regularization",
+    )
+    parser.add_argument(
+        "--max_iter",
+        type=int,
+        default=100,
+        help="Maximum number of iterations to converge",
+    )
+
     args = parser.parse_args()
 
     run = Run.get_context()
@@ -56,18 +83,23 @@ def main():
     # Data is located at:
     # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-    ds =TabularDatasetFactory.from_delimited_files("https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv")
-    
+    ds = TabularDatasetFactory.from_delimited_files(
+        "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+    )
+
     x, y = clean_data(ds)
 
     # TODO: Split data into train and test sets.
 
-    x_train, x_test, y_train, y_test =train_test_split(x,y,test_size=test_size, random_state=lucky_number)
+    x_train, x_test, y_train, y_test = train_test_split(
+        x, y, test_size=TEST_SIZE, random_state=LUCKY_NUMBER
+    )
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
